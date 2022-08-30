59f1337ecd342029da75e80df6b46d5c9a95051c
Hassan Khousheish <hassan@crunchrapps.com>
2022-08-30 22:30:25 +0200

    Add Tslint And Fix Project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+const bootstrap: () => Promise<void> = async(): Promise<void> => {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const port: number = 3000;
+
+  await app.listen(port);
+};
 bootstrap();
